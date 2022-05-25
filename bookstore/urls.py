6a2f2c87cfe764685964cc40be27bbe8be770a40
Shah rahman <toshah.rahman@gmail.com>
2022-05-25 20:51:11 +0300

    django fun 5

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('book/', include('books.urls')),
+    path('books', include('books.urls')),
     path("admin/", admin.site.urls),
 ]
